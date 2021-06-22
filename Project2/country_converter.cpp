88765d315b74b14fcdd2b2b8bc2c5367071541ee
Ryan Flickinger <rflickinger15@gmail.com>
2021-06-22 01:57:21 -0400

    Added input validation for country names, as well as assigned values to country trait variables.

--- Project2/country_converter.cpp
+++ Project2/country_converter.cpp
@@ -36,6 +36,7 @@ int country_traits[20][5] ={{2, 2, 2, 1, 2},
 
 //Home country name and traits
 string home_country;
+int home_country_index;
 int home_temp;
 int home_dist;
 int home_weight;
@@ -44,6 +45,7 @@ int home_driving_side;
 
 //Destination country name and traits
 string dest_country;
+int dest_country_index;
 int dest_temp;
 int dest_dist;
 int dest_weight;
@@ -100,12 +102,86 @@ void randomize_countries()
 
 int main()
 {
+	bool loop_bool = true;
+	//Introduction of program to user
 	cout << "Welcome prospective traveler! Lets take a look at your upcoming trip!" << endl;
-	cout << "Please enter the name of your home country! Press Enter to see the list of countries in my system." << endl;
+	cout << "Get ready to enter the name of your home country! Press Enter to see the list of countries in my system." << endl;
 	cin.get();
+
+	//Prints out list of countries in a column
 	for (string print_country : country_list) 
 	{
 		cout << print_country << endl;
 	}
+
+	//Take user input for home country
+	cout << "Please enter a country from the list exactly as it is written above" << endl;
+	getline(cin, home_country);
+
+	//Validate that user has input a country on the list and store its index
+	while (loop_bool)
+	{
+		bool is_valid = false;
+		for (int i = 0; i < sizeof(country_list)/sizeof(country_list[0]); i++)
+		{
+			if (home_country == country_list[i])
+			{
+				home_country_index = i;
+				i = sizeof(country_list) / sizeof(country_list[0]);
+				loop_bool = false;
+				is_valid = true;
+			}
+		}
+		if (!is_valid)
+		{
+			cout << "Oops! Looks like you chose an invalid option, please try again!" << endl;
+			getline(cin, home_country);
+		}
+	}
+	loop_bool = true;
+
+	//Assign home country traits
+	home_temp = country_traits[home_country_index][0];
+	home_dist = country_traits[home_country_index][1];
+	home_weight = country_traits[home_country_index][2];
+	home_road_side = country_traits[home_country_index][3];
+	home_driving_side = country_traits[home_country_index][4];
+	cout << "Awesome! You have chosen " << home_country << "!" << endl;
+
+	//Take destination input
+	cout << "Please enter a country from the list exactly as it is written above." << endl;
+	getline(cin, dest_country);
+
+	//Validate that user has input a country on the list and store its index
+	while (loop_bool)
+	{
+		bool is_valid = false;
+		for (int i = 0; i < sizeof(country_list) / sizeof(country_list[0]); i++)
+		{
+			if (dest_country == country_list[i])
+			{
+				dest_country_index = i;
+				i = sizeof(country_list) / sizeof(country_list[0]);
+				loop_bool = false;
+				is_valid = true;
+			}
+		}
+		if (!is_valid)
+		{
+			cout << "Oops! Looks like you chose an invalid option, please try again!" << endl;
+			getline(cin, dest_country);
+		}
+	}
+	loop_bool = true;
+	
+	//Assign destination country traits
+	dest_temp = country_traits[dest_country_index][0];
+	dest_dist = country_traits[dest_country_index][1];
+	dest_weight = country_traits[dest_country_index][2];
+	dest_road_side = country_traits[dest_country_index][3];
+	dest_driving_side = country_traits[dest_country_index][4];
+	cout << "Great job! You have chosen " << dest_country << "!" << endl;
+
+
 	return 0;
 }
